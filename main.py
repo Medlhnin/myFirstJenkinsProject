1d84579f2695e1f27f392c9ca90a421a7b7fe27c
MohamedBelfaquih <lahnin.010@gmail.com>
2025-08-13 01:42:21 +0000

    main.py changed for the sixth time

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import requests
 
 response = requests.get('https://api.github.com')
 print(f"GitHub API status: {response.status_code}")
+print(f"Response content: {response.json()}")
 print("Request completed successfully.")
